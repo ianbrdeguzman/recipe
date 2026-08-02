35a03b3197722420bb6eeba154bfd6efe82489da
chaos <ianbrdeguzman@gmail.com>
2026-08-01 21:53:05 -0400

    feat(db): add Drizzle Postgres integration

---
+++ src/drizzle/index.ts
@@ -0,0 +1,13 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import { users } from "./schema";
+
+const connectionString = process.env.DATABASE_URL!;
+
+// Disable prefetch as it is not supported for "Transaction" pool mode
+export const client = postgres(connectionString, { prepare: false });
+export const db = drizzle(client);
+
+export function getAllUsers() {
+  return db.select().from(users);
+}
